65655b37abc545dd39443bb4f68b8eb3985aee53
Christopher Kane <ckane@cs.stonybrook.edu>
2016-05-06 23:25:54 -0400

    distutils setup continues, added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+import sa
+
+setup(name = "SecAlgo",
+      version = "0.01",
+      author = "cmkane",
+      author_email = "ckane@cs.stonybrook.edu",
+      classifiers = ['Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.4',],
+      packages = ['sa'])
