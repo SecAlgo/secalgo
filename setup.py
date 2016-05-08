7a76e6623a79dfca3943b5e00a994a8b8930dd28
Christopher Kane <ckane@cs.stonybrook.edu>
2016-05-07 22:27:17 -0400

    updated .gitignore,setup\; added __init__.py's where necessary

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name = "SecAlgo",
       version = "0.01",
       author = "cmkane",
       author_email = "ckane@cs.stonybrook.edu",
+      url = "https://github.com/cmkane01/SecAlgoProject",
       classifiers = ['Programming Language :: Python :: 3',
                      'Programming Language :: Python :: 3.4',],
       packages = ['sa', 'sa.Misc'])
