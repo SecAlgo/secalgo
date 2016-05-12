883b0b8a00106857033d379bda930a9bb98520f2
Christopher Kane <ckane@cs.stonybrook.edu>
2016-05-12 17:03:37 -0400

    continuing work on ds_python_roles_pycrypto.py

--- ProtocolImplementations/Denning-Sacco/Python/ds_python_roles_pycrypto.py
+++ ProtocolImplementations/Denning-Sacco/Python/ds_python_roles_pycrypto.py
@@ -8,6 +8,25 @@ from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from Crypto import Random
 
+MIN_TCP_PORT = 1025
+MAX_TCP_PORT = 65500
+
+
 class pk_server():
+    def __init__(address):
+        self.pk = dict()
+        self.port = None:
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while self.port == None:
+            self.port = random.randint(MIN_TCP_PORT, MAX_TCP_PORT)
+            try:
+                self.list_socket.bind(self.address, self.port)
+            except socket.error:
+                self.port = None
+        #end while port assignment
+    # end __init__()
+
+    def start():
+        
     
 #end class pk_server
