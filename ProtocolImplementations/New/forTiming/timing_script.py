d63594ada636018c97eaa5d3cea17e2999af529c
Christopher Kane <ckane@cs.stonybrook.edu>
2017-05-16 09:48:06 -0400

    Timing script working; need to update protocols.

--- ProtocolImplementations/New/forTiming/timing_script.py
+++ ProtocolImplementations/New/forTiming/timing_script.py
@@ -1,7 +1,7 @@
 import sys, os, subprocess, time
 
 full_path = '/home/christopher/secalgo/ProtocolImplementations/New/forTiming/'
-result_path = 'home/christopher/secalgo/results/'
+result_path = '/home/christopher/secalgo/results/'
 
 da_ext = '.da'
 results_ext = '_results.txt'
@@ -10,10 +10,10 @@ error_ext = '_results.log'
 protocols = [
                   'ds-ft',
                   'ds-pk-ft',
-                  'ns-sk-ft',
-                  'ns-pk-ft',
-                  'or-ft',
-                  'wl-ft',
+                  #'ns-sk-ft',
+                  #'ns-pk-ft',
+                  #'or-ft',
+                  #'wl-ft',
                   #'ya-ft',
                   #'dhke-1-ft',
                   #'eap_archie-ft.da',
@@ -25,11 +25,11 @@ protocols = [
                  ]
 
 def time_exp01():
-    for fn in protocols:
-        print('Running:', fn, flush = True)
-        f_txt = open(result_path + fn + results.ext, 'w')
-        f_err = open(result_path + fn + error_ext, 'w')
-        child = subprocess.Popen(['python3', '-m', 'da', full_path + fn], 
+    for p in protocols:
+        print('Running:', p, flush = True)
+        f_txt = open(result_path + p + results_ext, 'w')
+        f_err = open(result_path + p + error_ext, 'w')
+        child = subprocess.Popen(['python3', '-m', 'da', full_path + p + da_ext], 
                                  bufsize= -1,
                                  stdout = f_txt,
                                  stderr = f_err, 
@@ -39,7 +39,7 @@ def time_exp01():
         #print(stdout, flush = True)
         f_txt.close()
         f_err.close()
-        print('Finished:', fn, flush = True)
+        print('Finished:', p, flush = True)
         time.sleep(1)
 #end time_exp01()
 
