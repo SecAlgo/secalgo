c0acefa6b3664310b8ec5b5164cc6f842dd0d543
Christopher Kane <ckane@cs.stonybrook.edu>
2017-05-15 22:29:13 -0400

    Added file names to list for experiment, added full_path as global (will only work on my linux installations), put call to Popen inside loop over elements of file name list.

--- ProtocolImplementations/New/forTiming/timing_script.py
+++ ProtocolImplementations/New/forTiming/timing_script.py
@@ -1,11 +1,21 @@
 import sys, os, subprocess
 
-def test():
-    child = subprocess.Popen(['python3', '-m', 'da', '/home/Christopher/secalgo/ProtocolsImplementations/New/ns-sk.da'], bufsize= -1, stdout = subprocess.PIPE,
-                     stderr = subprocess.PIPE, universal_newlines = True)
-    stdout, stderr = child.communicate()
-    print(stdout)
-#end test()
+full_path = '/home/Christopher/secalgo/ProtocolImplementations/New/forTiming'
+
+protocol_files = ['ds-ft.da', 'ds-pk-ft.da', 'ns-sk-ft.da', 'ns-pk-ft.da',
+                  'or-ft.da', 'wl-ft.da', 'ya-ft.da',  'dhke-1-ft.da', 
+                  'eke-ft.da', 'sdh-ft.eke', 'eap_archie-ft.da', 'tls1_2-ft.da',
+                  'kerberos-ft.da', 'iso9798-3-4-ft.da']
+
+def time_exp01(protocol):
+    for fn in protocol_files:
+        child = subprocess.Popen(['python3', '-m', 'da', full_path + fn], 
+                                 bufsize= -1, stdout = subprocess.PIPE,
+                                 stderr = subprocess.PIPE, 
+                                 universal_newlines = True)
+        stdout, stderr = child.communicate()
+        print(stdout)
+#end time_exp01()
 
 if __name__ == '__main__':
-    test()
+    time_exp01()
